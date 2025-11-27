928a918bb0983ae49b39d69b49f0fba5491d258c
Laura Le Gall <laura.maglegall@gmail.com>
2025-11-27 21:45:14 +0100

    mains js created to start

---
+++ main.js
@@ -0,0 +1,39 @@
+/*-----------------------------------------------
+ * Project Name: Memory Game
+ * File: script.js
+ * Description: Main JavaScript logic for the project
+ * Author: Laura
+ * Date: Novembre 2025
+ --------------------------------------------------*/
+
+/* ============================
+   GLOBAL VARIABLES
+   ============================ */
+const cards = ["assets/bulldozer"];
+echo(cards);
+
+
+
+
+/* ============================
+   DOM ELEMENTS
+   ============================ */
+// const button = document.querySelector('');
+
+
+
+
+/* ============================
+   MAIN FUNCTIONS
+   ============================ */
+
+
+
+/* ============================
+   EVENT LISTENERS
+   ============================ */
+
+
+/* ============================
+   INITIALIZATION
+   ============================ */
