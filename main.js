4d88b8a5f01e232cfe4aec94e7da9fd1bfbf7eac
Laura Le Gall <laura.maglegall@gmail.com>
2025-11-28 23:01:01 +0100

    both card sides

--- main.js
+++ main.js
@@ -7,7 +7,11 @@
  --------------------------------------------------*/
 /* 
 - shuffle les images au clic sur restart 
-- 
+- mettre un timer dés que permiere carte decouverte et derniere 
+- selectionner deux cartes maximum
+- si cartes identique ok on valide et elles restent apparentes
+- si cartes diiférentes, on valide pas et elles se retournent 
+- au clic sur une image ca prends sa valeur et sa la retourne 
 */
 
 
@@ -18,12 +22,8 @@
    ============================ */
 
 
-   const cardsFrame = document.querySelector(".cards"); // yellow frame wich contains all the cards = parent
+const cardsFrame = document.querySelector(".cards"); // yellow frame wich contains all the cards = parent
 
-  
-
-
-    
 const restartBtn = document.querySelector(".Rtg");
 
 const eachCard = document.querySelectorAll(".EachCard");
@@ -38,7 +38,7 @@ const cardsArray = ["assets/bulldozer.png", "assets/cementmixer.png","assets/cra
 /* ============================
    DOM ELEMENTS
    ============================ */
-// const button = document.querySelector('');
+
 
 
 
@@ -57,58 +57,75 @@ function shuffle() {
 function createCard() {
 cardsArray.forEach(function(card){
     
- //create the card with the picture 
+
+//create the div for both 
+    let bothCards = document.createElement("div");
+    bothCards.className = "both";
+
+
+//create the card with the picture 
     let newCard = document.createElement("img");
     newCard.src = card;
     newCard.alt = "Une image";
     newCard.className = "frontImage";
 
 
-    // add it to the dom 
-    cardsFrame.appendChild(newCard);  
-  
-    console.log(card);
+     //create the back of the card
+    let newCardBack = document.createElement("div");
+    newCardBack.className = "backImage";
     
-   
-    
-})
+     // add them to the dom 
+
+    bothCards.appendChild(newCard);  
+    bothCards.appendChild(newCardBack);  
+    cardsFrame.appendChild(bothCards);  
+    })
 }
 
 /* ============================
    EVENT LISTENERS
    ============================ */
 
-
-/* ============================
-   INITIALIZATION
-   ============================ */
-
-
-//---------------create the cards from the array 
-  createCard();
-//----------------reset the order of pictures 
-
-
-restartBtn.addEventListener("click", function(){
+// restart button
+   restartBtn.addEventListener("click", function(){
     const cardsFrame = document.querySelector(".cards"); // yellow frame wich contains all the cards = parent
     shuffle();
     cardsFrame.innerHTML ="";
     cardsArray.forEach(function(card){
     
 
-   //create the card with the picture 
+    //create the div for both 
+    let bothCards = document.createElement("div");
+    bothCards.className = "both";
+
+
+    //create the card with the picture 
     let newCard = document.createElement("img");
     newCard.src = card;
     newCard.alt = "Une image";
     newCard.className = "frontImage";
 
+
+     //create the back of the card
+    let newCardBack = document.createElement("div");
+    newCardBack.className = "backImage";
     
-     // add it to the dom 
-    cardsFrame.appendChild(newCard);  
-    
-    
-})
-    
+     // add them to the dom 
+
+    bothCards.appendChild(newCard);  
+    bothCards.appendChild(newCardBack);  
+    cardsFrame.appendChild(bothCards);  
+    })
 
 });
 
+
+/* ============================
+   INITIALIZATION
+   ============================ */
+//---------------create the cards from the array 
+  createCard();
+
+//----------------
+
+//flip card à faire
